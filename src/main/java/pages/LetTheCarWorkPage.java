7c423bba3c63cf7b0bfb257af6ed56367eb58ac6
KirillChertokQA <prefixspb@gmail.com>
2024-09-24 21:00:42 +0300

    started add new car

--- src/main/java/pages/LetTheCarWorkPage.java
+++ src/main/java/pages/LetTheCarWorkPage.java
@@ -1,6 +1,10 @@
 package pages;
 
+import dto.CarDto;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 
@@ -9,5 +13,31 @@ public class LetTheCarWorkPage extends BasePage{
     public LetTheCarWorkPage(WebDriver driver){
         setDriver(driver);
         PageFactory.initElements(new AjaxElementLocatorFactory(driver, 10),this);
+    }
+    @FindBy(id = "pickUpPlace")
+    WebElement inputLocation;
+    @FindBy(id = "make")
+    WebElement inputManufacture;
+    @FindBy(id = "model")
+    WebElement inputModel;
+    @FindBy(id = "year")
+    WebElement inputYear;
+    @FindBy(id = "fuel")
+    WebElement inputFuel;
+    @FindBy(id = "seats")
+    WebElement inputSeats;
+    @FindBy(id = "class")
+    WebElement inputCarClass;
+    @FindBy(id = "serialNumber")
+    WebElement inputSerialNumber;
+    @FindBy(id = "price")
+    WebElement inputPrice;
+    @FindBy(id = "about")
+    WebElement inputAbout;
+
+    public void typeAddNewCarForm(CarDto car) {
+        inputLocation.sendKeys(car.getCity());
+        pause(2);
+        driver.findElement(By.xpath("//div[@class='pac-item']")).click();
     }
 }
---
+++ src/test/java/tests/AddNewCarTests.java
@@ -0,0 +1,39 @@
+package tests;
+
+import dto.CarDto;
+import manager.ApplicationManager;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LetTheCarWorkPage;
+import utils.HeaderMenuItem;
+
+import static pages.BasePage.clickButtonsOnHeader;
+
+public class AddNewCarTests extends ApplicationManager {
+
+    LetTheCarWorkPage letTheCarWorkPage;
+    @BeforeMethod
+    public void startAddCar(){
+        new HomePage(getDriver());
+        letTheCarWorkPage = clickButtonsOnHeader((HeaderMenuItem.LET_THE_CAR_WORK));
+    }
+
+    @Test
+    public void addNewCarPositiveTest(){
+        CarDto car = CarDto.builder()
+                .city("Tel ")
+                .manufacture("Toyota")
+                .model("Prius.jpg")
+                .year("2019")
+                .fuel("")
+                .seats(5)
+                .carClass("C-class")
+                .serialNumber("1222")
+                .pricePerDay(1000)
+                .about("text")
+                //.carPhotoPath("Prius.jpg")
+                .build();
+        letTheCarWorkPage.typeAddNewCarForm(car);
+    }
+}
